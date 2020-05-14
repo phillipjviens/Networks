6f8d0a48ce334becdf54f85fd4ad6f88709a7bec
Phillip Viens <38144672+phillipjviens@users.noreply.github.com>
2020-05-14 13:13:57 -0700

    can now transfer binary files

--- http_svr.cpp
+++ http_svr.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
@@ -112,7 +113,7 @@ int main(int argc, char *argv[]){
 
             size_t delim = req.find_first_of("/");
             string req_type = req.substr(0,delim-1);
-            cout <<"REQUEST: " << req_type << endl;
+            //cout <<"REQUEST: " << req_type << endl;
 
             if (req_type != "GET"){
                 //bzero(buffer, 131072);
@@ -147,13 +148,105 @@ int main(int argc, char *argv[]){
             else{
                 size_t path_length = req.find("HTTP");
                 string path = req.substr(delim+1,path_length-6  );
-                cout << path.length() << endl;
-                if(FILE *file = fopen(path.c_str(), "r")){
-                    cout << path << endl;
+                size_t tp = path.find_last_of(".");
+                string type = path.substr(tp);
+                if(FILE *file = fopen(path.c_str(), "r+")){
+                    struct stat statbuff;
+                    char char_path[path.length()+1];
+                    strncpy(char_path,path.c_str(),path.length()+1);
+
+                    if(stat(char_path, &statbuff) == -1){
+                        perror("stat");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    int con_length = statbuff.st_size;
+
+                    string cl = to_string(con_length);
+                    string mod = ctime(&statbuff.st_mtime);
+
+                    string header = "HTTP/1.1 200 OK\r\n"
+                                     "Date: \r\n"
+                                     "Last-Modified: " + mod + 
+                                     "Content-Length: " + cl + "\r\n"
+                                     "Content-Type: " + type + "\r\n"
+                                     "\r\n";
+
                     
+                    //size_t fsize = ftell(file);
+                    //cout << header << endl;
+                    char head[header.length()+1];
+                    strncpy(head,header.c_str(),header.length()+1);
+                    fseek(file,0,SEEK_END);
+                    unsigned long filesize = ftell(file);
+                    fclose(file);
+                    //cout << "FILESIZE: " << filesize << endl;
+                    unsigned char *contents = (unsigned char *)malloc(filesize);
+                    file = fopen(char_path, "r+");
+                    int bytes_read = fread(contents, sizeof(unsigned char), filesize, file);
+                    fclose(file);
+                    //cout <<"BYTES READ: " << bytes_read << endl;
+                    //cout << "CHAR SIZE: " << sizeof(contents) << endl;
+
+                    //file = fopen("output.jpg", "w+");
+                    //int bytes_written = fwrite(contents, sizeof(unsigned char), filesize, file);
+                    //fclose(file);
+                    //cout << "BYTES_WRITTEN: " << bytes_written << endl;
+
+                    //file = fopen("output.jpg", "w+");
+                    //int bytes_written = fwrite(contents, sizeof(unsigned char), filesize, file);
+                   // fread(contents,sizeof(char),filesize,file);
+                    //cout <<contents<< endl;
+
+                    //fseek(file, 0, SEEK_END);
+                    //fseek(file,0,SEEK_SET);
+                    //fread(contents,con_length+1,1,file);
+                    //strncpy(contents,header.c_str(),hader.length());
+                    //fclose(file);
+
+                    //cout << contents << endl;
+                    int resp_length = header.length()+con_length;
+                    char resp[resp_length];
+                    int j = 0;
+                    for(int i = 0; i < header.length(); i ++){
+                        resp[i] = head[i];
+                        j++;
+                    }
+
+                    for(int i = 0; i < filesize;i++){
+                        resp[j+i] = contents[i];
+                        //cout << "THIS IS THE BEGINNING: " <<contents[0] << endl;
+                    }
+
+                    //cout << "SIZE OF RESPONSE: " << sizeof(resp) << endl;
+                    //for (int i = 0; i < sizeof(resp); i ++){
+                    //    cout << resp[i];
+                    //}
+
+                    //ifstream infile;
+                    //infile.open(path);
+                    //ofstream outfile;
+                    //outfile.open("new_"+path);
+                    //char c;
+                    //while(!infile.get(c)){
+                    //    outfile << c;
+                    //}
+                    //strcpy(resp,header.c_str());
+                    //strcat(resp,contents);
+                    //cout <<"RESPONSE: " << resp << endl;
+
+                    n = message("send", newsockfd, resp,sizeof(resp),0);
+                    if(n < 0){
+                        error("ERROR Writing to socket");
+                    }
+                    //n = message("send", newsockfd, contents, sizeof(contents),0);
+                    //if(n <0){
+                    //    error("ERROR Writing to socket");
+                    //}
+
                 }
                 else{
-                    cout << "NO FILE LOCATED: " << path << endl;
+                    //cout << "NO FILE LOCATED: " << path << endl;
                     string response = "HTTP/1.1 404 NOT FOUND\r\n"
                                       "Host: cs1.seattleu.edu\r\n"
                                       "Connection: close\r\n"
@@ -161,7 +254,7 @@ int main(int argc, char *argv[]){
                     int length = response.length()+1;
                     char resp[length];
                     bzero(resp,length);
-                    strncpy(resp,response.c_str(),length);
+                    //strcat(resp,response.c_str());
                     n = send(newsockfd, resp, length,0);
                     if(n < 0){
                         error("ERROR Writing to socket");
@@ -177,7 +270,7 @@ int main(int argc, char *argv[]){
     if (n < 0){
         error("ERROR reading from socket");
     }
-
+        close(newsockfd);
         
     }
 
